709346c1206648351fb0f4312c608ea83dc5b585
Corneliu Dascălu <corneliu.dascalu@gmail.com>
2022-04-06 21:35:58 +0300

    Build Splash screen UI

--- lib/splash.dart
+++ lib/splash.dart
@@ -9,15 +9,33 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   final radius = 300.0;
-  final circleColor = Colors.black;
+  final circleColor = const Color(0x3595989a);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: _circle(
-            radius: radius,
-            child: _circle(
-                radius: radius * 0.7, child: _circle(radius: radius * 0.4))));
+        body: Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          _circle(
+              radius: radius,
+              child: _circle(
+                  radius: radius * 0.7, child: _circle(radius: radius * 0.4))),
+          Padding(
+            padding: const EdgeInsets.only(right: 50),
+            child: Transform.scale(
+              child: Image.asset("assets/images/leaf-xxhdpi.png"),
+              scale: 0.5,
+            ),
+          ),
+          const Text(
+            "My      Weather",
+            style: TextStyle(fontSize: 35),
+          ),
+        ],
+      ),
+    ));
   }
 
   Widget _circle({double radius = 300, Widget? child}) {
@@ -30,7 +48,7 @@ class _SplashScreenState extends State<SplashScreen> {
               shape: BoxShape.circle,
               color: null,
               border: Border.all(
-                  color: circleColor, style: BorderStyle.solid, width: 2))),
+                  color: circleColor, style: BorderStyle.solid, width: 1))),
     );
   }
 }
