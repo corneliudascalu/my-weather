2050ee528a8eb472c1365b7790b5b462defa8e95
Corneliu Dascălu <corneliu.dascalu@gmail.com>
2022-04-08 16:41:14 +0300

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:weather/firebase_options.dart';
 import 'package:weather/login/sign_in.dart';
 import 'package:weather/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
